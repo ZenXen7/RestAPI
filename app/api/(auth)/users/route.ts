3bd6c373d942d375a7e5f4db7de12c8977bfd24e
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-03 11:39:38 +0800

    Delete Request

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -66,11 +66,28 @@ export const PATCH = async (request: Request) => {
 
 export const DELETE = async (request: Request) => {
     try {
-        await connect();
-        const users = await User.find();
         
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get("userId");
 
-    } catch (error) {
+        if(!userId) {
+            return new NextResponse(JSON.stringify({message: "ID not found."}), {status: 400});
+        }
+        if(!Types.ObjectId.isValid(userId)) {
+            return new NextResponse(JSON.stringify({message: "Invalid ID."}), {status: 400});
+        }
+
+        await connect();
+        const deletedUser = await User.findByIdAndDelete(
+            new Types.ObjectId(userId)
+        );
         
+        if(!deletedUser) {
+            return new NextResponse(JSON.stringify({message: "User not found in the database"}), {status: 400});
+        }
+
+        return new NextResponse(JSON.stringify({message: "User deleted successfully", user: deletedUser}), {status: 200});
+    } catch (error: any) {
+        return new NextResponse("Error in deleting user" + error.message, {status: 500});
     }
 }
